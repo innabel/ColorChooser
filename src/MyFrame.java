f719729d7334d31a9906180ed2f57e6fdcbf63cb
Inna Belopolskaia <innabelopolskaya@gmail.com>
2021-12-11 16:43:31 +0300

    Main Structure updates: - created additional files to track the changes (txt files, .gitignore); - created 2 main classes; - created buttons to change either the background color or text color within the frame; - added title to the window; - added a color chooser dialogue associated to the button; - added additional button to change the background color;

---
+++ src/MyFrame.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame implements ActionListener {
+
+    JButton buttonText;
+    JButton buttonBack;
+    JLabel label;
+    String font = "Freestyle Script";
+
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+        buttonText = new JButton("Pick text color");
+        buttonText.addActionListener(this);
+
+
+        buttonBack = new JButton("Pick background color");
+        buttonBack.addActionListener(this);
+
+
+        label = new JLabel();
+        label.setBackground(new Color(184, 224, 221));
+        label.setText("Thank you for checking this out. Change it now");
+        label.setFont(new Font(font, Font.PLAIN, 80));
+        label.setOpaque(true);
+
+        this.add(buttonText);
+        this.add(buttonBack);
+        this.add(label);
+        this.pack();
+        this.setTitle("Color Chooser");
+        this.setLocationRelativeTo(null); // to make the window appear in the middle
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if(e.getSource() == buttonText) {
+            JColorChooser colorChooser = new JColorChooser();
+
+            Color color = JColorChooser.showDialog(null, "Pick a color", Color.gray);
+            label.setForeground(color);
+        }
+
+        if(e.getSource() == buttonBack) {
+            JColorChooser colorChooser = new JColorChooser();
+
+            Color color = JColorChooser.showDialog(null, "Pick a color", Color.gray);
+            label.setBackground(color);
+        }
+    }
+}
